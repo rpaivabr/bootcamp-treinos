45ddecb1290e9c55adfd011ef6998b83dfbcc9d6
Rafael Paiva <r.paivabr@gmail.com>
2026-03-02 20:42:01 -0300

    feat: add create workout plan endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,92 +1,196 @@
-import 'dotenv/config';
+import "dotenv/config";
 
-import fastifyCors from '@fastify/cors';
-import fastifySwagger from '@fastify/swagger';
-import fastifySwaggerUI from '@fastify/swagger-ui';
-import Fastify from 'fastify';
-import { jsonSchemaTransform, serializerCompiler, validatorCompiler, ZodTypeProvider } from 'fastify-type-provider-zod';
-import z from 'zod';
+import fastifyCors from "@fastify/cors";
+import fastifySwagger from "@fastify/swagger";
+import fastifyApiReference from "@scalar/fastify-api-reference";
+import { fromNodeHeaders } from "better-auth/node";
+import Fastify from "fastify";
+import {
+  jsonSchemaTransform,
+  serializerCompiler,
+  validatorCompiler,
+  ZodTypeProvider,
+} from "fastify-type-provider-zod";
+import z from "zod";
 
-import { auth } from './lib/auth.js';
+import { NotFoundError } from "./errors/index.js";
+import { WeekDay } from "./generated/prisma/enums.js";
+import { auth } from "./lib/auth.js";
+import { CreateWorkoutPlan } from "./usecases/create-workout-plan.js";
 
 const app = Fastify({ logger: true });
 app.setSerializerCompiler(serializerCompiler);
 app.setValidatorCompiler(validatorCompiler);
 
-await app.register(fastifySwagger, {
-  openapi: {
-    info: {
-      title: 'Bootcamp Treinos API',
-      description: 'API para o bootcamp de treinos do FSC',
-      version: '1.0.0',
-    },
-    servers: [
-      { description: 'localhost', url: 'http://localhost:8081' }
-    ]
-  },
-  transform: jsonSchemaTransform
-});
-await app.register(fastifySwaggerUI, {
-  routePrefix: '/docs',
-});
-await app.register(fastifyCors, { 
-  origin: ['http://localhost:3000'],
+await app.register(fastifyCors, {
+  origin: ["http://localhost:3000"],
   credentials: true,
 });
 
-app.withTypeProvider<ZodTypeProvider>().route({
-  method: 'GET',
-  url: '/',
-  schema: {
-    description: "Hello world",
-    tags: ["Hello World"],
-    response: {
-      200: z.object({
-        message: z.string(),
-      })
-    }
+await app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "Bootcamp Treinos API",
+      description: "API para o bootcamp de treinos do FSC",
+      version: "1.0.0",
+    },
+    servers: [{ description: "localhost", url: "http://localhost:8081" }],
   },
-  handler: () => ({ message: "Hello world!" })
-})
+  transform: jsonSchemaTransform,
+});
+await app.register(fastifyApiReference, {
+  routePrefix: "/docs",
+  configuration: {
+    sources: [
+      {
+        title: "Bootcamp Treinos API",
+        slug: "bootcamp-treinos-api",
+        url: "/swagger.json",
+      },
+      {
+        title: "Auth API",
+        slug: "auth-api",
+        url: "/api/auth/open-api/generate-schema",
+      },
+    ],
+  },
+});
+app.withTypeProvider<ZodTypeProvider>().route({
+  method: "GET",
+  url: "/swagger.json",
+  schema: { hide: true },
+  handler: async () => app.swagger(),
+});
 
 app.route({
   method: ["GET", "POST"],
   url: "/api/auth/*",
   async handler(request, reply) {
     try {
-      // Construct request URL
       const url = new URL(request.url, `http://${request.headers.host}`);
-      
-      // Convert Fastify headers to standard Headers object
       const headers = new Headers();
       Object.entries(request.headers).forEach(([key, value]) => {
         if (value) headers.append(key, value.toString());
       });
-      // Create Fetch API-compatible request
       const req = new Request(url.toString(), {
         method: request.method,
         headers,
         ...(request.body ? { body: JSON.stringify(request.body) } : {}),
       });
-      // Process authentication request
       const response = await auth.handler(req);
-      // Forward response to client
       reply.status(response.status);
       response.headers.forEach((value, key) => reply.header(key, value));
       reply.send(response.body ? await response.text() : null);
     } catch (error) {
       app.log.error(`Authentication Error: ${error}`);
-      reply.status(500).send({ 
+      reply.status(500).send({
         error: "Internal authentication error",
-        code: "AUTH_FAILURE"
+        code: "AUTH_FAILURE",
       });
     }
-  }
+  },
+});
+
+app.withTypeProvider<ZodTypeProvider>().route({
+  method: "POST",
+  url: "/workout-plans",
+  schema: {
+    body: z.object({
+      name: z.string().trim().min(1),
+      workoutDays: z.array(
+        z.object({
+          name: z.string().trim().min(1),
+          weekDay: z.enum(WeekDay),
+          isRest: z.boolean().default(false),
+          estimatedDurationInSeconds: z.number().min(1),
+          exercises: z.array(
+            z.object({
+              order: z.number().min(0),
+              name: z.string().trim().min(1),
+              sets: z.number().min(1),
+              reps: z.number().min(1),
+              restTimeInSeconds: z.number().min(1),
+            }),
+          ),
+        }),
+      ),
+    }),
+    response: {
+      201: z.object({
+        id: z.uuid(),
+        name: z.string().trim().min(1),
+        workoutDays: z.array(
+          z.object({
+            name: z.string().trim().min(1),
+            weekDay: z.enum(WeekDay),
+            isRest: z.boolean().default(false),
+            estimatedDurationInSeconds: z.number().min(1),
+            exercises: z.array(
+              z.object({
+                order: z.number().min(0),
+                name: z.string().trim().min(1),
+                sets: z.number().min(1),
+                reps: z.number().min(1),
+                restTimeInSeconds: z.number().min(1),
+              }),
+            ),
+          }),
+        ),
+      }),
+      400: z.object({
+        error: z.string(),
+        code: z.string(),
+      }),
+      401: z.object({
+        error: z.string(),
+        code: z.string(),
+      }),
+      404: z.object({
+        error: z.string(),
+        code: z.string(),
+      }),
+      500: z.object({
+        error: z.string(),
+        code: z.string(),
+      }),
+    },
+  },
+  handler: async (request, reply) => {
+    try {
+      const session = await auth.api.getSession({
+        headers: fromNodeHeaders(request.headers),
+      });
+      if (!session) {
+        return reply.status(401).send({
+          error: "Unauthorized",
+          code: "UNAUTHORIZED",
+        });
+      }
+      const createWorkoutPlan = new CreateWorkoutPlan();
+      const result = await createWorkoutPlan.execute({
+        userId: session.user.id,
+        ...request.body,
+      });
+      reply.status(201).send(result);
+    } catch (error) {
+      app.log.error(error);
+      if (error instanceof NotFoundError) {
+        return reply.status(404).send({
+          error: error.message,
+          code: "NOT_FOUND_ERROR",
+        });
+      }
+      return reply.status(500).send({
+        error: "Internal server error",
+        code: "INTERNAL_SERVER_ERROR",
+      });
+    }
+  },
 });
 
 try {
   await app.listen({ port: Number(process.env.PORT) || 8081 });
-} catch(err) {
+} catch (err) {
   app.log.error(err);
   process.exit(1);
 }
